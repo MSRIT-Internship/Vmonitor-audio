ea27beb5135db8d070094a322a2343f2210399fa
Remote-RIT-1 <msritinternshipteam1@outlook.com>
2024-04-19 16:10:29 +0530

    major updates

--- audio.py
+++ audio.py
@@ -11,7 +11,11 @@ class AudIO:
         for path in self.paths:
             for file in path.iterdir():
                 try:
-                    audios.append(tuple(rosa.load(file)))
+                    audios.append((rosa.load(file),str(path).split('/')[0]))
+                    print(audios)
                 except Exception as e:
                     print(f"Warning: Failed to load {file} in {path}")
+                    print(e)
         return audios
+    
+        
